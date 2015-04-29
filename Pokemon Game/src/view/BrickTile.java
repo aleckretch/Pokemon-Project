983c975f3841f5d15ce39a1478ec36c89561525a
MoHeck <morganheck@dhcp-10-142-143-41.uawifi.arizona.edu>
2015-04-28 18:27:52 -0700

    Map

--- Pokemon Game/src/view/BrickTile.java
+++ Pokemon Game/src/view/BrickTile.java
@@ -7,7 +7,7 @@ public class BrickTile extends Tile{
 		
 	public BrickTile(int x, int y)
 	{
-		super("pics/brick_tile_red.jpg",x,y);
+		super("pics/brick_tile_red.png",x,y);
 	}
 	
 	public Image getImage()
--- Pokemon Game/src/view/Map.java
+++ Pokemon Game/src/view/Map.java
@@ -2,7 +2,6 @@ package view;
 
 
 import java.awt.Graphics;
-import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
